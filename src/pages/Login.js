72975739f76dcc5a30f7b3fd8d9a9263fcc90e98
Collins Adaigho <donsmatt44@gmail.com>
2023-04-25 12:38:53 +0100

    Login and Signup

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,34 @@
+import { FcGoogle } from "react-icons/fc";
+import Input from "../components/Input";
+import { Link } from "react-router-dom";
+
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="s-container">
+      <div className="s-signin">
+        <h2>Login to Muzira</h2>
+        <div className="s-inputContainer">
+          <Input type="email" placeholder="Email Address " />
+          <Input type="password" placeholder="Password " />
+        </div>
+        <p className="s-p s-purple">Forgot Password?</p>
+        <button className="s-btn s-btn-1" type="button">
+          LOGIN
+        </button>
+        <span className="s-span-1">or</span>
+        <button className="s-btn s-btn-2" type="button">
+          <FcGoogle />
+          <span>Continue With Google</span>
+        </button>
+        <p>
+          <span className="s-span-2">
+            Don't have an account?
+            <Link href="signup">Sign up</Link>
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,7 +1,51 @@
+import { FcGoogle } from "react-icons/fc";
+import Input from "../components/Input";
+import { Link } from "react-router-dom";
+
 const Signup = () => {
   return (
-    <div>Signup</div>
-  )
-}
+    <div className="s-container">
+      <div className="s-signup">
+        <h2>Sign in to Muzira</h2>
+        <button className="s-btn s-btn-2" type="button">
+          <FcGoogle />
+          <span>Continue With Google</span>
+        </button>
+        <span className="s-span-1">OR</span>
+        <div className="s-inputContainer">
+          <Input type="email" placeholder="Email Address " />
+          <Input type="password" placeholder="Create Password " />
+          <Input type="password" placeholder="Confirm Password " />
+          <Input type="text" placeholder="Enter Profile Name " />
+        </div>
+        <div>
+          <p>What is your date of birth</p>
+          <div>
+            <Input type="month" />
+          </div>
+        </div>
+        <div>
+          <p>Gender</p>
+          <div>
+            <input type="radio" name="gender" value="Male" />
+            <input type="radio" name="gender" value="Female" />
+            <input type="radio" name="gender" value="Prefer not to say" />
+          </div>
+        </div>
+        <p className="s-center">
+          By clicking on sign-up you agree fully to the terms and conditions and
+          privacy policy
+        </p>
+        <button className="s-btn s-btn-1" type="button">
+          SIGN UP
+        </button>
+        <span className="s-span-2">
+          Have an account?
+          <Link href="signup">Log in</Link>
+        </span>
+      </div>
+    </div>
+  );
+};
 
-export default Signup
+export default Signup;
